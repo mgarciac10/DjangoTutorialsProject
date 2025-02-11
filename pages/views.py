c64d5b11d9dc28a1989123c6b1c441ff5e4001c8
Mateo Garcia Carreno <mgarciac03@outlook.com>
2025-02-11 00:12:09 -0500

    Created the about page

--- pages/views.py
+++ pages/views.py
@@ -4,3 +4,16 @@ from django.views.generic import TemplateView
 # Create your views here.
 class homePageView(TemplateView):
     template_name = 'pages/home.html'
+
+class aboutPageView(TemplateView):
+    template_name = 'pages/about.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            'title': 'About us - Online Store',
+            'subtitle': 'About us',
+            'description': 'This is an about page...',
+            'author': 'Developed by: Mateo Garcia'
+        })
+        return context
