be77234f4321305974b2c814ba6b874463b9e746
Mateo Garcia Carreno <mgarciac03@outlook.com>
2025-02-10 23:50:04 -0500

    Created base and home templates, also updated the homePageView

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 # Create your views here.
-def homePageView(request):
-    return HttpResponse('Hello World!')
+class homePageView(TemplateView):
+    template_name = 'pages/home.html'
