f695697b73f79422bf5e0b1a8d0b8a405e72cb9e
Mateo Garcia Carreno <mgarciac03@outlook.com>
2025-02-11 23:59:16 -0500

    Created the product page and prodect show views and templates

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.views import View
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 class homePageView(TemplateView):
@@ -32,3 +34,37 @@ class contactPageView(TemplateView):
         })
         return context
 
+class Product:
+    products = [
+        {"id":"1", "name":"TV", "description":"Best TV", "price":"1000"}, 
+        {"id":"2", "name":"iPhone", "description":"Best iPhone", "price":"800"}, 
+        {"id":"3", "name":"Chromecast", "description":"Best Chromecast", "price":"50"}, 
+        {"id":"4", "name":"Glasses", "description":"Best Glasses", "price":"100"} 
+    ]
+
+class productIndexView(View):
+    template_name = 'products/index.html'
+
+    def get(self, request):
+        viewData = {} 
+        viewData["title"] = "Products - Online Store" 
+        viewData["subtitle"] =  "List of products" 
+        viewData["products"] = Product.products 
+
+        return render(request, self.template_name, viewData)
+    
+class productShowView(View):
+    template_name = 'products/show.html'
+
+    def get(self, request, id):
+
+        if int(id) > len(Product.products) or int(id) < 1:
+            return HttpResponseRedirect('/')
+
+        viewData = {} 
+        product = Product.products[int(id)-1] 
+        viewData["title"] = product["name"] + " - Online Store" 
+        viewData["subtitle"] =  product["name"] + " - Product information" 
+        viewData["product"] = product 
+ 
+        return render(request, self.template_name, viewData)
