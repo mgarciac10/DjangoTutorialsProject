b8701b6888011bef8103591020cbefc80843b826
Mateo Garcia Carreno <mgarciac03@outlook.com>
2025-02-16 18:15:51 -0500

    Created the cart template and view

--- pages/urls.py
+++ pages/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import HomePageView, AboutPageView, ContactPageView, ProductIndexView, ProductShowView, ProductCreateView, ProductCreatedView
+from .views import HomePageView, AboutPageView, ContactPageView, ProductIndexView, ProductShowView, ProductCreateView, ProductCreatedView, CartView, CartRemoveAllView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
@@ -8,5 +8,8 @@ urlpatterns = [
     path('products/', ProductIndexView.as_view(), name='index'),
     path('products/create', ProductCreateView.as_view(), name='form'),
     path('products/created', ProductCreatedView.as_view(), name='created'),
-    path('products/<str:id>', ProductShowView.as_view(), name='show')
+    path('products/<str:id>', ProductShowView.as_view(), name='show'),
+    path('cart/', CartView.as_view(), name='cart_index'), 
+    path('cart/add/<str:product_id>', CartView.as_view(), name='cart_add'), 
+    path('cart/removeAll', CartRemoveAllView.as_view(), name='cart_removeAll'), 
 ]
